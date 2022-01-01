40277f25345962fcbdeaef15627836b147b0b95e
Mohit Kakkar <mohit.kakkar87@gmail.com>
2022-01-02 04:01:17 +0530

    new changes of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+       
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
+        {
+            return await _context.Users.ToListAsync();
+
+        }
+
+         [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUse(int id)
+        {
+            return await _context.Users.FindAsync(id);
+
+        }
+    }
+}
